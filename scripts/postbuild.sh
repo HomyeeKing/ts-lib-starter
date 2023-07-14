d178811affa00a26e2ec89d9de51ec9ee61e86e9
HomyeeKing <HomyeeKing@gmail.com>
2023-07-14 11:33:34 +0800

    feat: update

---
+++ scripts/postbuild.sh
@@ -0,0 +1,1 @@
+cp src/typings.d.ts dist
---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  dts: true,
+  clean: true,
+  format: ["esm", "cjs"],
+  target: "node16",
+  skipNodeModulesBundle: true,
+  outExtension() {
+    return {
+      js: `.js`,
+    }
+  },
+})
